a92126472be3882e448c9fa14879cd6665ca053f
Muhammad Awais <fa22-bsse-201@lgu.edu.pk>
2024-12-16 01:05:55 +0500

    Add files via upload
    A replica of the dedafu website built using Tailwind Css, HTML, and JavaScript. This project aims to demonstrate my understanding of front-end development and Tailwind framework.

---
+++ defado.js
@@ -0,0 +1,93 @@
+$('.responsive').slick({
+    
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.custom-prev'), // Custom previous button 
+    nextArrow: $('.custom-next'), // Custom next button
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 350,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    
+    ]
+});
+
+const navDialogue = document.getElementById('nav-dialogue');
+const barsIcon = document.getElementById('barsIcon');
+function handleMenu() {
+    navDialogue.classList.toggle('hidden');
+    barsIcon.classList.toggle('hidden');
+
+}
+
+   
+    document.addEventListener('DOMContentLoaded', function() { 
+      const elements = document.querySelectorAll('.animate-me');
+      
+  
+      function checkVisibility() { 
+          elements.forEach(element => { 
+              const position = element.getBoundingClientRect();
+              if (position.top < window.innerHeight) {
+                  element.classList.add('show'); 
+                  
+              } else { 
+                  element.classList.remove('show');
+                  
+              } 
+          });
+      } 
+      
+      window.addEventListener('scroll', checkVisibility); 
+      checkVisibility(); // Initial check in case elements are already in view
+  });
+  document.addEventListener('DOMContentLoaded', function() { 
+    const escapeOut = document.querySelectorAll('.escape');
+    
+
+    function checkVisibility() { 
+        escapeOut.forEach(element => { 
+            const position = element.getBoundingClientRect();
+            if (position.top < window.innerHeight) {
+               
+                element.classList.add('escape-out'); 
+            } else { 
+              element.classList.remove('escape-out'); 
+                
+            } 
+        });
+    } 
+    
+    window.addEventListener('scroll', checkVisibility); 
+    checkVisibility(); // Initial check in case elements are already in view
+});
